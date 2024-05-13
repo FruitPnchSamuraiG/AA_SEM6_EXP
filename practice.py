c9d98de98715d542eeb772887339c93b3190245b
Hriday Ranka <hridayr1234@gmail.com>
2024-05-13 07:27:13 +0530

    updated practice

--- practice.py
+++ practice.py
@@ -1,56 +1,56 @@
-import random
 
-def randomized_quicksort_hoare(arr):
-    if len(arr) <= 1:
-        return arr, 0
+
+class Node:
+    def __init__(self, value, left, right, parent, compare):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+        self.compare = compare
+        
+def toggle_compare(current_compare):
+    if current_compare == 0:
+        return 1
     else:
-        pivot_index = random.randint(0, len(arr) - 1)
-        pivot = arr[pivot_index]
-        left, right = [], []
-        swaps = 0
-
-        for i in range(len(arr)):
-            if i != pivot_index:
-                if arr[i] <= pivot:
-                    left.append(arr[i])
-                else:
-                    right.append(arr[i])
-                    swaps += 1  # Increment swaps when moving to right partition
-
-        left_sorted, left_swaps = randomized_quicksort_hoare(left)
-        right_sorted, right_swaps = randomized_quicksort_hoare(right)
-
-        return left_sorted + [pivot] + right_sorted, swaps + left_swaps + right_swaps
-
-def quicksort_lomuto(arr):
-    if len(arr) <= 1:
-        return arr, 0
-    else:
-        pivot = arr[-1]
-        i = 0
-        swaps = 0
-
-        for j in range(len(arr) - 1):
-            if arr[j] <= pivot:
-                arr[i], arr[j] = arr[j], arr[i]
-                i += 1
-                swaps += 1  # Increment swaps when swapping elements
-
-        arr[i], arr[-1] = arr[-1], arr[i]
-        pivot_index = i
-
-        left_sorted, left_swaps = quicksort_lomuto(arr[:pivot_index])
-        right_sorted, right_swaps = quicksort_lomuto(arr[pivot_index + 1:])
-
-        return left_sorted + [pivot] + right_sorted, swaps + left_swaps + right_swaps
-
-# Example usage and counting swaps:
-arr = [1, 5, 3, 8, 2, 7, 4, 6, 9]
-sorted_arr_hoare, swaps_hoare = randomized_quicksort_hoare(arr.copy())
-sorted_arr_lomuto, swaps_lomuto = quicksort_lomuto(arr.copy())
-
-print("Sorted Array (Hoare Partition Scheme):", sorted_arr_hoare)
-print("Number of Swaps (Hoare Partition Scheme):", swaps_hoare)
-
-print("\nSorted Array (Lomuto Partition Scheme):", sorted_arr_lomuto)
-print("Number of Swaps (Lomuto Partition Scheme):", swaps_lomuto)
+        return 0
+    
+def insert_node(value):
+    global root_node
+    current_parent = root_node
+    current_compare = current_parent.compare
+    current_value= value
+    parent_found  = False
+    
+    while not parent_found:
+        if current_value[current_compare]< current_parent.value[current_compare]:
+            if current_parent.left is None:
+                new_node = Node(current_value, None, None, current_parent, toggle_compare(current_compare))
+                current_parent.left = new_node
+                parent_found = True
+            else:
+                current_parent = current_parent.left 
+                current_compare = toggle_compare(current_compare)
+        else:
+            if current_parent.right is None:
+                new_node = Node(current_value, None, None, current_parent, toggle_compare(current_compare))
+                current_parent.right = new_node
+                parent_found = True
+            else:
+                current_parent = current_parent.right 
+                current_compare = toggle_compare(current_compare)
+    return root_node
+    
+    
+def print_tree(node, indent=0, prefix="root"):
+    if node is not None:
+        print('|'*indent + '--'*indent + f"{'' if indent == 0 else ' '}" +  f"{prefix}: ({node.value[0]}, {node.value[1]})")
+        print_tree(node.left, indent+1, prefix='left')
+        print_tree(node.right, indent+1, prefix='right')
+        
+input_values = [[3, 6], [17, 15], [13, 15], [6, 12], [9, 1], [2, 7], [10, 19]]
+root_node = Node(input_values[0], None, None, None, 0)
+# compare 0 -> means x
+# compare 1 -> means y
+for i in range(1, len(input_values)):
+    root_node = insert_node(input_values[i])
+print_tree(root_node)
